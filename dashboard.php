5b279d61d402114ebb38bf7d957883e5e0338b8e
Nithya Rajkumar <73163361+nithyar15@users.noreply.github.com>
2021-07-11 22:04:20 +0300

    Delete dashboard.php

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-	include("connection.php");
-    include("functions.php");
-    more_dashboard($con);
-    echo "<br><button onclick=window.location.href='main.php'>Create another trip</button>";
-    echo "<br><br><button onclick=window.location.href='login.php'>Logout</button>";
-?>
